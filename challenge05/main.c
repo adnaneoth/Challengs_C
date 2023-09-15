7e8e4a095972f165ffc0c92e11d66a891929e43c
adnaneoth <adnaneothmane64@gmail.com>
2023-09-15 12:46:05 +0200

    challenge5

---
+++ challenge05/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int M1,M2,N1,N2,DMN;
+    printf("donne les cordonnes de M  ");
+    printf("\npoint M1 : ");
+    scanf("%d",&M1);
+    printf("\npoint M2 : ");
+    scanf("%d",&M2);
+    printf("\ndonne les cordonnes de N  ");
+    printf("\npoint N1 : ");
+    scanf("%d",&N1);
+    printf("\npoint N2 : ");
+    scanf("%d",&N2);
+    DMN=sqrt(pow((N1-M1),2) + pow((N2-M2),2));
+    printf("Distance entre deux points est : %d ",DMN);
+    return 0;
+}
