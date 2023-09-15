eada2120ccdfec9ca51fde01e8dc11a4c1e8ccc6
adnaneoth <adnaneothmane64@gmail.com>
2023-09-15 18:21:26 +0200

    challenge11

---
+++ challenge11/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,somme ;
+	printf("donne a : ");
+	scanf("%d",&a);
+	printf("donne b : ");
+	scanf("%d",&b);
+	if(a==b){
+		somme = (a+b)*3;
+		printf("le triple de leur somme est : %d",somme);
+	}else{
+		somme = a+b;
+		printf("la somme est : %d",somme);
+	}
+    return 0;
+}
