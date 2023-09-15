cf6f7d87a0394210b3d09f88767e91720b004880
adnaneoth <adnaneothmane64@gmail.com>
2023-09-15 15:51:42 +0200

    challeng8

---
+++ challenge8/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("donnez moi un nombre entier\n");
+    scanf("%d",&a);
+
+
+    printf("\nle nombre octal de %d est : %o\n",a,a);
+     printf("le nombre hexadecimal de %d est : %x\n",a,a);
+
+
+
+
+    return 0;
+}
