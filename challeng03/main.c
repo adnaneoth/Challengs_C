01f6a90ad9e9b143b504fdd0c389b4c3d7324fed
adnaneoth <adnaneothmane64@gmail.com>
2023-09-15 11:53:43 +0200

    chaleng3

---
+++ challeng03/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      int a,b;
+    printf("donne a : ");
+    scanf("%d",&a);
+    printf("donne b : ");
+    scanf("%d",&b);
+    printf("\na + b = %d",a+b);
+    printf("\na - b = %d",a-b);
+    printf("\na / b = %d",a/b);
+    printf("\na * b = %d",a*b);
+    printf("\na MOD b = %d",a%b);
+    return 0;
+}
