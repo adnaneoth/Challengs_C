d0297620dd43cacf570d1242aa1e51ae8160addd
adnaneoth <adnaneothmane64@gmail.com>
2023-09-16 15:34:59 +0200

    challenge 21

--- challenge19/main.c
+++ challenge19/main.c
@@ -9,15 +9,22 @@ int main()
 
 
 
-   int number = rand()%7+1;
-	switch(number){
-		case 1 : printf("lundi\n");break;
-		case 2 : printf("mardi\n");break;
-		case 3 : printf("mercredi\n");break;
-		case 4 : printf("jeudi\n");break;
-		case 5 : printf("vendredi\n");break;
-		case 6 : printf("samedi\n");break;
-		case 7 : printf("dimanch\n");break;
+   int num = rand()%6;
+	switch(num){
+		case 0 : printf("lundi\n");
+		break;
+		case 1 : printf("mardi\n");
+		break;
+		case 2 : printf("mercredi\n");
+		break;
+		case 3 : printf("jeudi\n");
+		break;
+		case 4 : printf("vendredi\n");
+		break;
+		case 5 : printf("samedi\n");
+		break;
+		case 6 : printf("dimanch\n");
+		break;
 	}
 }
     return 0;
---
+++ challenge21/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    int n,i,j;
+    	printf("donnez moi le nombre de ligne : ");
+        scanf("%d",&n);
+   for (i=1;i<=n;i++) {
+        for (j=1;j<=n-i;j++) {
+            printf(" ");
+        }
+        for (j= 1; j <= 2*i-1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+
+
+    return 0;
+}
